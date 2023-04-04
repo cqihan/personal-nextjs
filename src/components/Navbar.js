e485b3cb1a3704e0eccf88a76af0edef685df87e
cqihan <qihanchan_97@hotmail.com>
2023-04-05 01:01:45 +0800

    fix Link issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 // import { Varela_Round } from 'next/font/google'
 import styles from '@/styles/Home.module.css';
-import Link from 'next/link';
 import { useRouter } from "next/router";
 
 // const varela_font = Varela_Round({ subsets: ['latin'] })
