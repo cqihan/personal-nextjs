75e4e9c2ae879b26003b0b5b5272bfbf83b7eaf3
cqihan <qihanchan_97@hotmail.com>
2023-04-04 20:56:51 +0800

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import "@fontsource/varela-round";
+import '@/styles/animations.scss';
+import Navbar from '@/components/Navbar';
+import { NextUIProvider } from '@nextui-org/react';
+
+import '@/styles/Navbar.scss'
+import '@/styles/index.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log('pageProps',pageProps)
+  return (
+    <>
+    {pageProps.statusCode === 404 ? (
+      <Component {...pageProps} />
+    ) : (
+      <>
+        <Navbar />
+        <Component {...pageProps} />
+      </>
+    )}
+    </>
+  )
 }
