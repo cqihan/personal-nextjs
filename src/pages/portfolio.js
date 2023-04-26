1c5eac4726f2b89d321c9f3d95325b7a49309cd2
cqihan <qihanchan_97@hotmail.com>
2023-04-26 18:36:38 +0800

    add difficulty lvl

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 // import { PersonalCard } from '@/components/CardTemplate_1';
 import { Card, Col, Row, Grid, Text, Popover, Button } from "@nextui-org/react";
 import { Projects1 } from '@/components/Portfolio_Card1';
+import { AiFillFire } from "react-icons/ai";
 
 
 export default function Portfolio() {
@@ -71,6 +72,29 @@ export default function Portfolio() {
                     <Text css={{ p: "10px" }} color="#fff" size={12}>Sass: Syntactically Awesome Style Sheets</Text>
                   </Popover.Content>
                 </Popover>
+              </div>
+            </div>
+          </div>
+          
+          <div className="port-card-2">
+            <h4>Spice Level (difficulty)</h4>
+
+            <div className="port-card-2-item">
+              <div className="port-card-2-item-1">
+                <AiFillFire className="port-card-2-item-icon1" />
+                Low
+              </div>
+              <div className="port-card-2-item-2">
+                <AiFillFire className="port-card-2-item-icon1" />
+                Medium
+              </div>
+              <div className="port-card-2-item-3">
+                <AiFillFire className="port-card-2-item-icon1" />
+                Hot
+              </div>
+              <div className="port-card-2-item-4">
+                <AiFillFire className="port-card-2-item-icon1" />
+                Extra Hot
               </div>
             </div>
           </div>
